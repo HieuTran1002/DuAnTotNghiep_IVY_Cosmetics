4d7040280dca3322f87e4130a95a4e467a4c7c33
Kienhtph16079 <kienhtph16079@fpt.edu.vn>
2024-03-13 22:34:48 +0700

    testdm

---
+++ src/main/java/com/example/datn/entity/DanhMuc.java
@@ -0,0 +1,44 @@
+package com.example.datn.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.UUID;
+
+@Table(name = "danhmuc")
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class DanhMuc {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id")
+    private UUID id;
+
+    @Column(name = "ma")
+    private String ma;
+
+    @Column(name = "ten")
+    private String ten;
+
+    @Column(name = "trangthai")
+    private int trangThai;
+
+    @ManyToOne
+    @JoinColumn(name = "idsanpham")
+    private SanPham sanPham;
+}
